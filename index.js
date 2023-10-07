b58fcb17d579842737d9821fbb5075e5a4ecff09
Yousef-AN <kingyousef922@gmail.com>
2023-10-07 22:42:27 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(record) {
+  const winningEntry = record.find(entry => entry.result === "W");
+  if (winningEntry) {
+    return winningEntry.year;
+  } else {
+    return undefined;
+  }
+}
